4efb86f5382c8515455ac7a410ee5bfc882e05c2
Ryan Gavigan <gavigan.ryan@gmail.com>
2015-11-07 14:48:44 -0500

    Initial setup to add clone system call

--- defs.h
+++ defs.h
@@ -118,6 +118,7 @@ void            userinit(void);
 int             wait(void);
 void            wakeup(void*);
 void            yield(void);
+void            clone(void(*fcn)(void*), void *arg, void *stack);
 
 // swtch.S
 void            swtch(struct context**, struct context*);
--- syscall.c
+++ syscall.c
@@ -55,7 +55,7 @@ int
 argptr(int n, char **pp, int size)
 {
   int i;
-  
+
   if(argint(n, &i) < 0)
     return -1;
   if((uint)i >= proc->sz || (uint)i+size > proc->sz)
@@ -98,6 +98,7 @@ extern int sys_unlink(void);
 extern int sys_wait(void);
 extern int sys_write(void);
 extern int sys_uptime(void);
+extern int sys_clone(void);
 
 static int (*syscalls[])(void) = {
 [SYS_fork]    sys_fork,
@@ -121,6 +122,7 @@ static int (*syscalls[])(void) = {
 [SYS_link]    sys_link,
 [SYS_mkdir]   sys_mkdir,
 [SYS_close]   sys_close,
+[SYS_clone]   sys_clone;
 };
 
 void
