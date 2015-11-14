3592b8ea9474a59e9e1d250e6fa339a91857e3c5
Ryan Gavigan <gavigan.ryan@gmail.com>
2015-11-14 15:37:08 -0500

    zombie

--- uthread.c
+++ uthread.c
@@ -16,9 +16,48 @@ void lock_acquire(lock_t *lock) {
 
 void lock_release(lock_t *lock) {
 }
-/*
-int thread_join(int pid) {
+
+int
+thread_join(int pid)
+{
+    if(proc->thread == 1)
+        return -1;
+    struct proc *p;
+    int havekids;
+
+    acquire(&ptable.lock);
+    for(;;){
+        // Scan through table looking child
+        havekids = 0;
+        for(p = ptable.proc; p < &ptable.proc[NPROC]; p++){
+            if(p->pid == pid){
+                if(p->parent != proc || p->thread != 0)
+                    return -1;
+            }
+            havekids = 1;
+            if(p->state == ZOMBIE){
+              // Found one.
+              pid = p->pid;
+              p->state = UNUSED;
+              p->pid = 0;
+              p->parent = 0;
+              p->name[0] = 0;
+              p->killed = 0;
+              release(&ptable.lock);
+              return pid;
+          }
+      }
+        // No point waiting if we don't have any children.
+        if(!havekids || proc->killed){
+            release(&ptable.lock);
+            return -1;
+    }
+        // Wait for children to exit.  (See wakeup1 call in proc_exit.)
+        sleep(proc, &ptable.lock);  //DOC: wait-sleep
+    }
+    return 0;
 }
 
 int thread_create(void (*start_routine)(void *), void *arg) {
-}*/
+    return 0;
+}
